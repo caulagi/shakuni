61fec8a8265effe67fb158f98b2469950b744b7d
Pradip P Caulagi <caulagi@gmail.com>
2014-06-13 15:55:15 +0530

    Another stab at server error

--- app/bets/controllers.py
+++ app/bets/controllers.py
@@ -50,10 +50,8 @@ def init(application):
         try:
             group_match = GroupMatch.objects.get(id=id)
         except GroupMatch.DoesNotExist:
-            abort(403, "Not a valid match")
-        print group_match
+            return abort(403, "Not a valid match")
         bets = Bet.objects(group_match = group_match)
-        print bets
         return render_template("bets/list.html", bets=bets)
 
     @bets_blueprint.route('/<id>/create', methods=['GET', 'POST'])
